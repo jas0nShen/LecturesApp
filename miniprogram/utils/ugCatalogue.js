d9ebdf8cfaf04da2c059b679f71bbde5c4b6859a
SpongeBbob <1912447100@qq.com>
2026-07-08 22:31:20 +0800

    Add HKU history undergraduate courses

--- miniprogram/utils/ugCatalogue.js
+++ miniprogram/utils/ugCatalogue.js
@@ -2,7 +2,7 @@
 const courseShards = require('./ugCourseShards');
 const catalogue = {
   "generatedFrom": "programme_year_semester_courses_2026",
-  "generatedAt": "2026-07-08T14:20:28.028Z",
+  "generatedAt": "2026-07-08T14:28:43.726Z",
   "universities": [
     {
       "id": "HKU",
@@ -749,9 +749,10 @@ const catalogue = {
       "studyPeriod": "4-year",
       "type": "Single Degree Programme",
       "officialUrl": "https://admissions.hku.hk/programmes/undergraduate-programmes/bachelor-of-arts",
-      "sourceStatus": "programme_summary_only",
-      "courseCount": 1,
-      "codedCourseCount": 0
+      "sourceStatus": "course_codes_available",
+      "courseCount": 179,
+      "codedCourseCount": 179,
+      "courseSourceUrl": "https://arts.hku.hk/wp-content/uploads/2026/02/BA_syl_2025-26.pdf"
     },
     {
       "id": "HKU-UG-6054-14",
@@ -9167,9 +9168,9 @@ const catalogue = {
       "code": "HISTORY",
       "nameEn": "History",
       "nameZh": "History",
-      "courseCount": 1,
-      "codedCourseCount": 0,
-      "officialUrl": "https://admissions.hku.hk/programmes/undergraduate-programmes/bachelor-of-arts"
+      "courseCount": 180,
+      "codedCourseCount": 179,
+      "officialUrl": "https://arts.hku.hk/current-students/undergraduate/regulations/"
     },
     {
       "id": "HKU-UG-6054-14-M1",
@@ -15935,12 +15936,12 @@ const catalogue = {
   "courseShardCounts": {
     "CITYU": 1966,
     "CUHK": 131,
-    "HKU": 955,
+    "HKU": 1134,
     "HKUST": 121,
     "LINGNAN": 721,
     "POLYU": 166
   },
-  "codedCourseCount": 4060
+  "codedCourseCount": 4239
 };
 Object.defineProperty(catalogue, 'courses', {
   enumerable: true,
--- miniprogram/utils/ugCourseShards.js
+++ miniprogram/utils/ugCourseShards.js
@@ -10,7 +10,7 @@ const loaders = {
 const counts = {
   "CITYU": 1966,
   "CUHK": 131,
-  "HKU": 955,
+  "HKU": 1134,
   "HKUST": 121,
   "LINGNAN": 721,
   "POLYU": 166
