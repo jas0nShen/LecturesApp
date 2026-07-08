2cb82bc704fc78688beeec32d3e24ee4ac61be2f
SpongeBbob <1912447100@qq.com>
2026-07-08 18:13:59 +0800

    Add HKU comparative literature undergraduate courses

--- miniprogram/utils/ugCatalogue.js
+++ miniprogram/utils/ugCatalogue.js
@@ -2,7 +2,7 @@
 const courseShards = require('./ugCourseShards');
 const catalogue = {
   "generatedFrom": "programme_year_semester_courses_2026",
-  "generatedAt": "2026-07-08T10:06:09.135Z",
+  "generatedAt": "2026-07-08T10:11:42.081Z",
   "universities": [
     {
       "id": "HKU",
@@ -649,9 +649,10 @@ const catalogue = {
       "studyPeriod": "4-year",
       "type": "Single Degree Programme",
       "officialUrl": "https://admissions.hku.hk/programmes/undergraduate-programmes/bachelor-of-arts",
-      "sourceStatus": "programme_summary_only",
-      "courseCount": 1,
-      "codedCourseCount": 0
+      "sourceStatus": "course_codes_available",
+      "courseCount": 71,
+      "codedCourseCount": 71,
+      "courseSourceUrl": "https://arts.hku.hk/wp-content/uploads/2026/02/BA_syl_2025-26.pdf"
     },
     {
       "id": "HKU-UG-6054-9",
@@ -9112,9 +9113,9 @@ const catalogue = {
       "code": "COMPARATIVE-LITERATURE",
       "nameEn": "Comparative Literature",
       "nameZh": "Comparative Literature",
-      "courseCount": 1,
-      "codedCourseCount": 0,
-      "officialUrl": "https://admissions.hku.hk/programmes/undergraduate-programmes/bachelor-of-arts"
+      "courseCount": 72,
+      "codedCourseCount": 71,
+      "officialUrl": "https://arts.hku.hk/current-students/undergraduate/regulations/"
     },
     {
       "id": "HKU-UG-6054-9-M1",
@@ -15930,12 +15931,12 @@ const catalogue = {
   "courseShardCounts": {
     "CITYU": 1966,
     "CUHK": 131,
-    "HKU": 441,
+    "HKU": 512,
     "HKUST": 121,
     "LINGNAN": 721,
     "POLYU": 166
   },
-  "codedCourseCount": 3546
+  "codedCourseCount": 3617
 };
 Object.defineProperty(catalogue, 'courses', {
   enumerable: true,
