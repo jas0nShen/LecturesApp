2228b330a265c64ba92aaf2ba1e5e62cd918af05
SpongeBbob <1912447100@qq.com>
2026-07-29 00:53:46 +0800

    Release 1.0.13 with expanded CUHK curricula

--- miniprogram/utils/releaseInfo.js
+++ miniprogram/utils/releaseInfo.js
@@ -1,6 +1,6 @@
 module.exports = {
-  version: '1.0.12',
+  version: '1.0.13',
   target: '香港高校课程规划助手正式版',
   dataMode: '体验版 / 正式版离线数据',
-  releaseNote: '扩充 HKU 商科、法学及 2026 Science Master Class、BSc 官方课程清单。'
+  releaseNote: '扩充 CUHK 人文、社会科学与经济学官方课程清单，并保留规则未闭合 Programme 的只读边界。'
 };
